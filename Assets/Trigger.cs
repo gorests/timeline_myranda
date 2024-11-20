8d85cf42f1b16369a769becd1adf38411c1f93cc
Myranda <myrandaroy@gmail.com>
2024-11-20 12:15:29 -0400

    fixed animation

---
+++ Assets/Trigger.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class Trigger : MonoBehaviour
+{
+
+    public PlayableDirector timeline;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        timeline.Play();
+    }
+}
+   
